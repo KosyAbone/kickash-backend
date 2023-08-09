0827ad896d549565ba8940feb92919b373635b2b
Kosiso Abone <abonekosiso@gmail.com>
2023-08-09 15:28:08 -0400

    All APIs complete

--- src/Models/Articles.js
+++ src/Models/Articles.js
@@ -11,3 +11,8 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
+
+
+//Used this command below to import json file to Mongo atlas
+/*mongoimport --uri "mongodb+srv://byteforce:Byteforce77mdev@kickashcluster.ec6taqa.mongodb.net/KickAsh" \
+--collection articles --file article.json --jsonArray*/
