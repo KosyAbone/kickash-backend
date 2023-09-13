554f6d2b81425397bbd51dee2e1fe77899e42594
Kosiso Abone <abonekosiso@gmail.com>
2023-09-12 23:30:12 -0400

    codr changes

--- src/Controllers/profileController.js
+++ src/Controllers/profileController.js
@@ -17,12 +17,12 @@ const updateUserProfile = async (req, res) => {
         return res.status(403).json({ message: 'Forbidden. You are not authorized to update this profile.' });
       }
   
-      Update user profile using the userId
-      await User.findByIdAndUpdate(userId, req.body);
-      res.status(200).json({ message: 'Profile updated successfully', success: true });
-    } catch (error) {
-      res.status(500).json({ message: 'Internal server error', success: false });
-    }
+    //   Update user profile using the userId
+    //   await User.findByIdAndUpdate(userId, req.body);
+    //   res.status(200).json({ message: 'Profile updated successfully', success: true });
+    // } catch (error) {
+    //   res.status(500).json({ message: 'Internal server error', success: false });
+    // }
   };
 
 module.exports = { getUserProfile, updateUserProfile };
