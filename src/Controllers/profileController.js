0d1172fdc621766201e6e63ac04a143d89c0bde7
Kosiso Abone <abonekosiso@gmail.com>
2023-09-19 21:14:15 -0400

    added changes

--- src/Controllers/profileController.js
+++ src/Controllers/profileController.js
@@ -18,11 +18,11 @@ const updateUserProfile = async (req, res) => {
       }
   
       //Update user profile using the userId
-      await User.findByIdAndUpdate(userId, req.body);
-      res.status(200).json({ message: 'Profile updated successfully', success: true });
-    } catch (error) {
-      res.status(500).json({ message: 'Internal server error', success: false });
-    }
+    //   await User.findByIdAndUpdate(userId, req.body);
+    //   res.status(200).json({ message: 'Profile updated successfully', success: true });
+    // } catch (error) {
+    //   res.status(500).json({ message: 'Internal server error', success: false });
+    // }
   };
 
 module.exports = { getUserProfile, updateUserProfile };
